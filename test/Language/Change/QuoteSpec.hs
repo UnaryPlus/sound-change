f040f16efb5d526eb6f9f4029c33d5ed35cbd4fc
UnaryPlus <ombspring@gmail.com>
2023-07-01 15:30:45 -0400

    add files for testing

---
+++ test/Language/Change/QuoteSpec.hs
@@ -0,0 +1,6 @@
+module Language.Change.QuoteSpec where
+
+import Test.Hspec (Spec, {- it, describe, shouldBe -})
+
+spec :: Spec
+spec = return ()
---
+++ test/Language/ChangeSpec.hs
@@ -0,0 +1,6 @@
+module Language.ChangeSpec where
+
+import Test.Hspec (Spec, {- it, describe, shouldBe -})
+
+spec :: Spec
+spec = return ()
