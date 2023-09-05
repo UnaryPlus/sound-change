4a10488ca203e117931031ab4529cb0fd3c6c131
UnaryPlus <ombspring@gmail.com>
2023-09-05 10:50:12 -0400

    finish library

--- src/Language/Change/Quote.hs
+++ src/Language/Change/Quote.hs
@@ -7,42 +7,32 @@ Stability  : experimental
 -}
 
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# OPTIONS_GHC -Wno-missing-fields #-}
+{-# LANGUAGE TupleSections #-}
 module Language.Change.Quote where
 
+import Data.Data (Data)
 import Data.Void (Void)
-import Data.List.NonEmpty (NonEmpty(..))
+import Data.List.NonEmpty (NonEmpty(..), toList)
 import Data.Char (isAlphaNum, isAsciiUpper, isSpace)
 import Control.Monad (void)
 
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Set (Set)
+
 import qualified Text.Megaparsec as M
-import qualified Text.Megaparsec.Char as MC
 import qualified Text.Megaparsec.Char.Lexer as Lexer
 import qualified Control.Monad.Combinators.NonEmpty as NE
 
 import Control.Monad.Reader (ReaderT(..), local)
 
-{-
-import qualified Data.Map as Map
-import qualified Data.Set as Set
-import Data.Map (Map)
-import Data.Set (Set)
-import Data.List.NonEmpty (NonEmpty(..), toList)
-import Data.Data (Data)
-import Data.Void (Void)
-import Control.Monad (void)
-import Data.Char (isAlphaNum, isAsciiUpper, isSpace)
-
 import qualified Language.Haskell.TH as TH
-import Language.Haskell.TH.Quote (QuasiQuoter(..), dataToExpQ)
-import Language.Haskell.TH (Q, Exp)
+import Language.Haskell.TH.Quote(QuasiQuoter(..), dataToExpQ)
+import Language.Haskell.TH.Lib (appE, conE, varE)
 import Data.Generics.Aliases (extQ)
-
-import Language.Change (Change(..), Env(..), Pattern(..), PSet(..))
--}
+import Language.Change (Change(..), Env(..), PSet(..), Pattern(..))
 
 {-
 ENVIRONMENTS
@@ -72,7 +62,7 @@ newlines are required between statements
 within statements, they are allowed anywhere within { }
 
 SPACES
-spaces are allowed anywhere (except in antiquoted identifiers)
+spaces are allowed anywhere, except in replacements and antiquoted identifiers
 
 ALLOWED PHONEMES
 everything except whitespace, capital letters, and reserved symbols (, > / ; { } [ ] % _ ! ? *) 
@@ -91,27 +81,34 @@ data CharS
   = Lit Char
   | AntiQ String -- antiquoted identifier
   | CList [Char] -- does not exist in syntax; used for compilation of antiquotes
+  deriving (Data)
 
 newtype SetS = SetS (NonEmpty CharS)
+  deriving (Data)
 
 data PSetS = PSetS SetS Bool
+  deriving (Data)
 
 data PatternS
   = OneS PSetS
   | OptionalS PSetS
   | ManyS PSetS
+  deriving (Data)
 
 data EnvS = EnvS [PatternS] [PatternS]
+  deriving (Data)
 
 type Pairing a b = NonEmpty (a, b)
 type SoundList = NonEmpty CharS
 type EnvList = NonEmpty EnvS
-newtype Rep = Rep String
+newtype Rep = Rep { convertRep :: String }
+  deriving (Data)
 
 data StatementS
   = Simple SoundList Rep EnvList
   | SoundSplit SoundList (Pairing Rep EnvList)
   | EnvSplit (Pairing SoundList Rep) EnvList 
+  deriving (Data)
 
 type ChangeS = NonEmpty StatementS
 
@@ -170,7 +167,7 @@ patternS = do
 
 envS :: Parser EnvS
 envS = EnvS
-  <$> M.many patternS
+  <$> (reverse <$> M.many patternS)
   <* symbol '_'
   <*> M.many patternS
 
@@ -230,198 +227,86 @@ initialState input position =
     , M.stateParseErrors = []
     }
 
+ch :: QuasiQuoter
+ch = QuasiQuoter 
+  { quoteExp = quoteChange
+  , quotePat = undefined 
+  , quoteType = undefined
+  , quoteDec = undefined 
+  }
 
-
-
-{-
-data CharS
-  = Lit Char
-  | CSet (Set Char)
-  | AntiQ String
-  deriving (Data)
-
-type SetS = NonEmpty CharS
-
-data PSetS
-  = PSetS SetS Bool
-  deriving (Data)
-
-data PatternS
-  = OneS PSetS
-  | OptionalS PSetS
-  | ManyS PSetS
-  deriving (Data)
-
-data EnvS
-  = EnvS [PatternS] [PatternS]
-  deriving (Data)
-
-data ChangeS
-  = SimpleS (Pairing SetS String) (NonEmpty EnvS)
-  | SplitS (Pairing SetS (Pairing String (NonEmpty EnvS)))
-  deriving (Data)
-
-type Parser = M.Parsec Void String
-
-special :: [Char]
-special = ">/,[]{}?*!_"
-
-isSound, isIdentifier :: Char -> Bool
-isSound c = not (c `elem` special || isAsciiUpper c || isSpace c)
-isIdentifier c = isAlphaNum c || c == '\'' || c == '_'
-
-spaces, spacesN :: Parser ()
-spaces = void (M.takeWhileP Nothing (== ' '))
-spacesN = void (M.takeWhileP Nothing isSpace)
-
-symbol, symbolN :: Char -> Parser ()
-symbol c = M.single c >> spaces
-symbolN c = M.single c >> spacesN
-
---allows newlines
-replacement :: Parser String
-replacement = M.takeWhileP Nothing isSound <* spacesN
-
-charS :: Parser CharS
-charS = lit <|> oneChar <|> multipleChar
-  where
-    lit = Lit <$> M.satisfy isSound <* spaces
-
-    oneChar = AntiQ . pure <$> M.satisfy isAsciiUpper <* spaces
-
-    multipleChar = AntiQ
-      <$ symbol '['
-      <*> M.takeWhile1P Nothing isIdentifier
-      <* spaces
-      <* symbol ']'
-
-setS :: Parser SetS
-setS = (:| []) <$> charS
-  <|> M.between (symbol '{') (symbol '}') (NE.some charS)
-
-pSetS :: Parser PSetS
-pSetS = do
-  s <- setS
-  b <- False <$ symbol '!'
-    <|> return True
-  return (PSetS s b)
-
-patternS :: Parser PatternS
-patternS = do
-  s <- pSetS
-  OptionalS s <$ symbol '?'
-    <|> ManyS s <$ symbol '*'
-    <|> return (OneS s)
-
---allows newlines
-envS :: Parser EnvS
-envS = EnvS
-  <$> M.many patternS
-  <* symbol '_'
-  <*> M.many patternS
-  <* spacesN
-
-simpleS :: Parser ChangeS
-simpleS = SimpleS
-  <$> NE.sepBy1 change (symbolN ',')
-  <* symbolN '/'
-  <*> NE.sepBy1 envS (symbolN ',')
-  where
-    change = (,) <$> setS <* symbol '>' <*> replacement
-
-splitS :: Parser ChangeS
-splitS = SplitS <$> NE.some clause
-  where
-    clause = (,) <$> setS <* spacesN <*> NE.some change
-    change = (,) <$ symbolN '>' <*> replacement
-      <* symbolN '/' <*> NE.sepBy1 envS (symbolN ',')
-
---TODO: fix setLoc
-
-setLoc :: (Int, Int) -> Parser ()
-setLoc (line, col) =
-  M.updateParserState \state ->
-    let posState = M.statePosState state
-        sourcePos = M.pstateSourcePos posState
-        sourcePos' = sourcePos
-          { M.sourceLine = M.mkPos line
-          , M.sourceColumn = M.mkPos col
-          }
-        posState' = posState { M.pstateSourcePos = sourcePos' }
-    in state { M.statePosState = posState' }
-
-total :: Parser a -> Parser a
-total p = spacesN >> p <* M.eof
-
-pat :: QuasiQuoter
-pat = QuasiQuoter { quoteExp = quote (M.many patternS) 'convertPatterns }
-
-env :: QuasiQuoter
-env = QuasiQuoter { quoteExp = quote envS 'convertEnv }
-
-sim :: QuasiQuoter
-sim = QuasiQuoter { quoteExp = quote simpleS 'convertChange }
-
-spl :: QuasiQuoter
-spl = QuasiQuoter { quoteExp = quote splitS 'convertChange }
-
-quote :: Data a => Parser a -> TH.Name -> String -> Q Exp
-quote p convert input = do
+quoteChange :: String -> TH.ExpQ
+quoteChange input = do
   loc <- TH.location
   let file = TH.loc_filename loc
       (line, col) = TH.loc_start loc
-      p' = setLoc (line, col) >> total p
-  case M.runParser p' file input of
+      state = initialState input (M.SourcePos file (M.mkPos line) (M.mkPos col))
+      parse = spaces >> (changeS <* M.eof)
+  
+  case snd (M.runParser' (runReaderT parse False) state) of
     Left errors -> fail (M.errorBundlePretty errors)
-    Right change -> let
-      change' = dataToExpQ (const Nothing `extQ` antiquote) change
-      in [| $(TH.varE convert) $(change') |]
+    Right change -> 
+      let change' = dataToExpQ (const Nothing `extQ` antiquote) change
+      in appE (TH.varE 'convertChangeS) change'
 
-antiquote :: CharS -> Maybe (Q Exp)
+antiquote :: CharS -> Maybe TH.ExpQ
 antiquote = \case
   Lit _ -> Nothing
-  CSet _ -> Nothing
-  AntiQ n -> Just [| $(TH.conE 'CSet) $(TH.varE (TH.mkName ("set" ++ n))) |]
+  AntiQ s -> Just (appE (conE 'CList) (varE (TH.mkName ("set" ++ s))))
+  CList _ -> Nothing  
 
-convertSet :: SetS -> Set Char
-convertSet = foldMap convert
-  where
-    convert = \case
-      Lit c -> Set.singleton c
-      CSet s -> s
-      AntiQ _ -> undefined
+badAntiQ :: a
+badAntiQ = error "Language.Change.Quote: Unexpected AntiQ at runtime.\nThis error should never happen."
 
-convertPSet :: PSetS -> PSet Char
-convertPSet (PSetS s b) = PSet (convertSet s) b
+convertCharList :: CharS -> [Char]
+convertCharList = \case
+  Lit c -> [c]
+  AntiQ _ -> badAntiQ
+  CList cs -> cs
 
-convertPattern :: PatternS -> Pattern Char
-convertPattern = \case
-  OneS s -> One (convertPSet s)
-  OptionalS s -> Optional (convertPSet s)
-  ManyS s -> Many (convertPSet s)
+convertCharSet :: CharS -> Set Char
+convertCharSet = \case
+  Lit c -> Set.singleton c
+  AntiQ _ -> badAntiQ 
+  CList cs -> Set.fromList cs
 
-convertPatterns :: [PatternS] -> [Pattern Char]
-convertPatterns = map convertPattern
+convertSetS :: SetS -> Set Char
+convertSetS (SetS cs) = foldMap convertCharSet cs
 
-convertEnv :: EnvS -> Env Char
-convertEnv = \case
-  EnvS ps1 ps2 -> Env (reverse (map convertPattern ps1)) (map convertPattern ps2)
+convertPSetS :: PSetS -> PSet Char
+convertPSetS (PSetS set b) = PSet (convertSetS set) b 
 
-convertChange :: ChangeS -> Change Char
-convertChange = \case
-  SimpleS mapping envs ->
-    Simple (foldr split1 Map.empty mapping) (map convertEnv (toList envs))
+convertPatternS :: PatternS -> Pattern Char
+convertPatternS = \case
+  OneS set -> One (convertPSetS set)
+  OptionalS set -> Optional (convertPSetS set)
+  ManyS set -> Many (convertPSetS set)
 
-  SplitS mapping ->
-    Split (foldr (\(set, pairs) -> split2 (set, foldr split3 [] pairs)) Map.empty mapping)
+convertEnvS :: EnvS -> Env Char
+convertEnvS (EnvS e1 e2) = Env (map convertPatternS e1) (map convertPatternS e2)
 
-  where
-    split1 :: (SetS, a) -> Map Char a -> Map Char a
-    split1 (set, x) m = foldr (\c -> Map.insert c x) m (convertSet set)
+convertEnvPair :: (Rep, EnvList) -> [(String, Env Char)]
+convertEnvPair (rp, envs) =
+  let rp' = convertRep rp
+  in map (\env -> (rp', convertEnvS env)) (toList envs)
 
-    split2 :: (SetS, [a]) -> Map Char [a] -> Map Char [a]
-    split2 (set, xs) m = foldr (\c -> Map.insertWith (++) c xs) m (convertSet set)
+convertStatementS :: StatementS -> [(Char, [(String, Env Char)])]
+convertStatementS = \case
+  Simple sounds rp envs ->
+    let pairs = convertEnvPair (rp, envs) 
+    in map (, pairs) (concatMap convertCharList sounds)
 
-    split3 :: Foldable t => (a, t EnvS) -> [(a, Env Char)] -> [(a, Env Char)]
-    split3 (x, envs) m = foldr (\e -> ((x, convertEnv e) :)) m envs
--}
+  SoundSplit sounds envPairs -> 
+    let pairs = concatMap convertEnvPair envPairs 
+    in map (, pairs) (concatMap convertCharList sounds)
+    
+  EnvSplit soundPairs envs ->
+    let envs' = map convertEnvS (toList envs)
+    in concatMap (\(sounds, rp) -> 
+          let rp' = convertRep rp
+              pairs = map (rp', ) envs'
+          in map (, pairs) (concatMap convertCharList sounds))
+        soundPairs
+
+convertChangeS :: ChangeS -> Change Char
+convertChangeS stmts = Change (Map.fromListWith (++) (concatMap convertStatementS stmts))
